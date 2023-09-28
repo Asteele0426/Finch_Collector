a29afafd0abec8ff4660c0b42471bd1749d93428
Ashley Nicole <ajacobsohn86@users.noreply.github.com>
2023-09-28 12:57:28 -0400

    part2

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
-  {'name': 'Lolo', 'breed': 'house finch', 'description': 'red and brown', 'age': 3},
-  {'name': 'Sachi', 'breed': 'american goldfinch', 'description': 'bright yellow with black wings', 'age': 2},
-]
 # Create your views here.
-
 def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  # We pass data to a template very much like we did in Express!
-  return render(request, 'finches/index.html', {
-    'finches': finches
-  })
+    finches = Finch.objects.all() # Retrieve all finches
+    return render(request, 'finches/index.html', { 
+        'finches': finches 
+    })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 
+    'finch': finch 
+    })
